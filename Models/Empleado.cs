fa172382882f7fbe607daf749e736090420981bb
urios <urios@M1000>
2025-07-06 17:15:46 -0600

    Agregar clase Prueba para diferencias en PR Ajuste de propiedad GeneroEmpleado

---
+++ Models/Empleado.cs
@@ -0,0 +1,16 @@
+﻿namespace TallerMecanico.Models
+{
+    public class Empleado
+    {
+        public int IdEmpleado { get; set; }
+        public string Nombre { get; set; }
+        public string Direccion { get; set; }
+        public string Telefono { get; set; }
+        public string ApellidoPaterno { get; set; }
+        public string ApellidoMaterno { get; set; }
+        public string Email { get; set; }
+        public int Edad { get; set; }
+        public string GeneroEmpleado { get; set; } 
+    }
+
+}
